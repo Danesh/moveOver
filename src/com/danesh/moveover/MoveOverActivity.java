3d1ccbe98846a1256faa04e7e884acb12d24f049
Danesh <danesh@danesh-computer.(none)>
2011-08-07 16:10:42 -0400

    Added listview

--- src/com/danesh/moveover/MoveOverActivity.java
+++ src/com/danesh/moveover/MoveOverActivity.java
@@ -1,19 +1,34 @@
 package com.danesh.moveover;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Environment;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.CompoundButton;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.ToggleButton;
 import android.widget.CompoundButton.OnCheckedChangeListener;
 
-public class MoveOverActivity extends Activity implements OnCheckedChangeListener {
+public class MoveOverActivity extends Activity implements OnCheckedChangeListener,OnClickListener {
     String sdcard = Environment.getExternalStorageDirectory().toString()+"/";
     String sourceFolder = sdcard+"DCIM"+"/";
     String destFolder = sdcard+"synca"+"/";
     TextView source,dest;
+    ToggleButton service;
+    ListView myList;
+    Button add;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,8 +38,21 @@ public class MoveOverActivity extends Activity implements OnCheckedChangeListene
         dest = (TextView)findViewById(R.id.dest);
         source.setText(sourceFolder);
         dest.setText(destFolder);
-        ToggleButton service = (ToggleButton)findViewById(R.id.toggleButton1);
+        service = (ToggleButton)findViewById(R.id.toggleButton1);
         service.setOnCheckedChangeListener(this);
+        myList = (ListView)findViewById(R.id.listView1);
+        add = (Button)findViewById(R.id.button1);
+        add.setOnClickListener(this);
+        setAdapter();
+    }
+
+    private void setAdapter() {
+        ArrayList<String> results = new ArrayList<String>();
+        SharedPreferences myPrefs = this.getSharedPreferences("storedArray", MODE_PRIVATE);
+        for (String item : myPrefs.getAll().keySet()){
+            results.add(item + " -> " + myPrefs.getAll().get(item));
+        }
+        myList.setAdapter(new ArrayAdapter<String>(this, R.layout.row, R.id.label, results));
     }
 
     @Override
@@ -41,5 +69,14 @@ public class MoveOverActivity extends Activity implements OnCheckedChangeListene
     
     public static void print(String msg){
         System.out.println("Danny "+msg);
+    }
+
+    @Override
+    public void onClick(View arg0) {
+        SharedPreferences myPrefs = this.getSharedPreferences("storedArray", MODE_PRIVATE);
+        SharedPreferences.Editor prefsEditor = myPrefs.edit();
+        prefsEditor.putString(source.getText().toString(), dest.getText().toString());
+        prefsEditor.commit();
+        setAdapter();
     }
 }
